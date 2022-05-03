66fc7113b5fd4daca45297edf481454691e19f3b
Lena Ringstad Olsen <lenaringstado@gmail.com>
2022-05-03 19:07:36 +0200

    Lagt til utils

---
+++ R/utils.R
@@ -0,0 +1,71 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+#' Compound assignment pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%<>\%}} for details.
+#'
+#' @name %<>%
+#' @rdname compound_assignment_pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %<>%
+#' @usage lhs \%<>\% rhs
+NULL
+
+#' Exposition pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%$\%}} for details.
+#'
+#' @name %$%
+#' @rdname exposition_pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %$%
+#' @usage lhs \%$\% rhs
+NULL
+
+#' Data pronouns
+#'
+#' See \code{rlang::\link[rlang:tidyeval-data]{.data}} for details
+#'
+#' @name .data
+#' @rdname pronoun
+#' @keywords internal
+#' @export
+#' @importFrom rlang .data
+#' @usage .data
+NULL
+
+#' Use name stored in variable
+#'
+#' See \code{rlang::\link[rlang:nse-force]{:=}} for details
+#'
+#' @name :=
+#' @rdname force_names
+#' @keywords internal
+#' @export
+#' @importFrom rlang :=
+#' @usage .data
+NULL
+
+#' Subtract months to a date without exceeding the last day of the new month
+#'
+#' See \code{lubridate::\link[lubridate:subtract_month]{\%m-\%}} for details.
+#'
+#' @name %m-%
+#' @rdname subtract_month
+#' @keywords internal
+#' @export
+#' @importFrom lubridate %m-%
+#' @usage e1 \%m-\% e2 e1
+NULL
