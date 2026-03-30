0440bc2cf6668b5223e20e3cfb7e5a7ec6cc419d
Lena Ringstad Olsen <lenaringstado@gmail.com>
2026-03-30 10:12:59 +0200

    Henter navn tidligere i autorapport. (#36)
    Co-authored-by: Lena Ringstad Olsen <lena.ringstad.olsen@helse-nord.no>

--- dev/oppsettLena.R
+++ dev/oppsettLena.R
@@ -20,12 +20,13 @@ library(rygg)
 RegDataV3AVN <- rapbase::loadRegData(registryName = 'data',
                                      query='SELECT * FROM allevarnum')
 
-RegDataRaa <- RyggRegDataV2V3(datoFra = '2007-01-01')
+RegDataRaa <- RyggRegDataV2V3(datoFra = '2025-01-01')
 RegData <- RyggPreprosess(RegData =RegDataRaa)
 
 
-RegData[which(RegData$TidlOprAntall>10), c('PasientID', 'OpDato', 'ShNavn', 'TidlOprAntall')]
-RegData$ShNavn
+# trombProfyl, trombProfylLettKI
+RyggFigAndelerGrVar(RegData=RegData, valgtVar='trombProfylLettKI', erMann='', preprosess = 0)
+
 
 # Aleris Drammen (4211881)
 # Aleris Drammen (107240) V2
